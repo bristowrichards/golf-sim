b18e8e624ad40ce74a1d46054540e00b618f889b
Bristow Richards <57153542+bristowrichards@users.noreply.github.com>
2024-06-19 14:01:23 -0400

    Create cards.py

---
+++ src/cards.py
@@ -0,0 +1,18 @@
+# import
+
+class Card:
+    def __init__(self) -> None:
+        pass
+
+    def eval(self) -> None:
+        pass
+
+class Deck:
+    def __init__(self) -> None:
+        pass
+
+    def shuffle(self) -> None:
+        pass
+
+    def deal(self) -> Card:
+        pass
