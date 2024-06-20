8fa48d011d192a79999a4bac2dc679a3032b0016
Bristow Richards <57153542+bristowrichards@users.noreply.github.com>
2024-06-20 10:32:24 -0400

    add hand attr to Player

--- src/player.py
+++ src/player.py
@@ -1,4 +1,4 @@
-# import
+import cards
 
 # the dimensions of choice seem to be:
 # 1. valuation of unknown cards
@@ -10,14 +10,21 @@
 # choices: 'flip', 
 
 class Player:
-    def __init__(self, name, pos, strategy='basic', risk_penalty=0, 
-                 knowledge_bonus=0, forgetfulness=0) -> None:
+    def __init__(self, name:str, pos:int, strategy:str='basic', 
+                 risk_penalty:float=0, knowledge_bonus:float=0, 
+                 forgetfulness:float=0) -> None:
         self.name = name
         self.pos = pos
+        self.hand = list()
         self.strategy = strategy # encoding strategy
         self.risk_penalty = risk_penalty
         self.knowledge_bonus = knowledge_bonus
         self.forgetfulness = forgetfulness # encoding forgetfulness
+    
+    def add_to_hand(self, card) -> None:
+        assert isinstance(card, cards.Card)
+        assert len(self.hand) < 4
+        self.hand.append(card)
 
     def play_basic(self, state) -> str:
         # placeholder for having per-strategy play logic
@@ -27,4 +34,10 @@ class Player:
         pass
 
     def __repr__(self) -> str:
-        return f'{self.name} is player {self.pos}'
+        return f'{self.name} (p{self.pos})'
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
