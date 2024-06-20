ff7222b87764c78cf2bbed8b29bff9cc1c56bcb7
Bristow Richards <57153542+bristowrichards@users.noreply.github.com>
2024-06-20 10:32:47 -0400

    add deal_hands to Game

--- src/game.py
+++ src/game.py
@@ -9,16 +9,17 @@ class Game:
         for p in players:
             assert isinstance(p, player.Player)
         self.players = players
-        self.num_players = len(players)
+        self.num_players = len(players) # is this necessary
         self.deck = cards.Deck()
         self.round = 1 # round in this context is one loop through players
         self.player_turn = 1 # turn is the individual player's turn
         self.label = label
         self.table = None
 
-    def deal(self) -> None:
-        # for p in player
-        pass
+    def deal_hands(self) -> None:
+        for i in range(4):
+            for p in self.players:
+                p.add_to_hand(self.deck.deal())
 
     def __repr__(self) -> str:
         return f'Game: round={self.round}, player_turn={self.player_turn}, label={self.label}'
@@ -27,9 +28,15 @@ def main():
     p1 = player.Player('Alice', 1)
     p2 = player.Player('Bob', 2)
     mygame = Game([p1, p2])
-    for p in mygame.players:
-        for i in range(4):
-            print(mygame.deck.deal())
+    
+    print(mygame.deck)
+    print(mygame.players[0].hand)
+    print(mygame.players[1].hand)
+    mygame.deal_hands()
+    print(mygame.deck)
+    print(mygame.players[0].hand)
+    print(mygame.players[1].hand)
+    
 
 if __name__ == '__main__':
     main()
