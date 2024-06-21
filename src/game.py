c622f7da37bcf24244f7f590e195250fdee00ac4
Bristow Richards <57153542+bristowrichards@users.noreply.github.com>
2024-06-21 11:08:38 -0400

    Update game.py

--- src/game.py
+++ src/game.py
@@ -4,7 +4,7 @@ import cards
 
 class Game:
     # yadda
-    def __init__(self, players:list, label=None) -> None:
+    def __init__(self, players:list, id=None) -> None:
         assert isinstance(players, list)
         for p in enumerate(players):
             assert isinstance(p[1], player.Player)
@@ -13,27 +13,31 @@ class Game:
         self.players = players
         self.num_players = len(players) # is this necessary
         self.deck = cards.Deck()
+        self.discard = cards.Discard()
         self.round = 0 # round in this context is one loop through players
         self.player_turn = 0 # turn is the individual player's turn
-        self.label = label
-        self.table = None
+        self.id = id
 
     def deal_hands(self) -> None:
         for i in range(4):
             for p in self.players:
                 p.add_to_hand(self.deck.deal())
 
+    def start_game(self) -> None:
+        self.deal_hands()
+        for p in self.players:
+            p.peek()
+        self._flip()
+    
+    def _flip(self) -> None:
+        self.discard.stack(self.deck.deal())
+
     def __repr__(self) -> str:
         return (f'Game: round={self.round}, player_turn={self.player_turn} '+
-                '({self.players[self.player_turn].name}), label={self.label}')
+                f'({self.players[self.player_turn].name}), id={self.id}')
 
 def main():
-    p1 = player.Player('Alice')
-    p2 = player.Player('Bob')
-    p3 = player.Player('Charlie')
-    mygame = Game([p1, p2, p3])
-    
-    print(mygame.players[1].pos)
+    pass
 
 if __name__ == '__main__':
     main()
